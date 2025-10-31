ba8144944b7f5aa569f6a39c0b57529383bec1ae
keon24 <keon24@users.noreply.github.com>
2025-10-30 20:22:20 -0700

    Reconnect backend repository

---
+++ users/auth.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from core.config import settings
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+OUATH2_SCHEME = OAuth2PasswordBearer(tokenurl="token")
+
+# create the token and copy the users data and set an expiration for token then update and encode jwt
+def create_access_token(data:str):
+    to_encode = data.copy() 
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# Verify the access token and raise errors if token expired
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
